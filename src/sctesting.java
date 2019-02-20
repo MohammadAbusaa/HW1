15b561244c5d5533ab1c68a2f4b1656912d4790b
MohammadAbusaa <47559635+MohammadAbusaa@users.noreply.github.com>
2019-02-20 22:18:17 +0300

    :)

---
+++ src/sctesting.java
@@ -0,0 +1,33 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+
+class sctesting {
+
+	shopcart c1;
+	@Test
+	void test() {
+		c1=new shopcart();
+		assertTrue(0==c1.itemscount());
+	}
+	@Test
+	void test2() {
+		c1=new shopcart();
+		c1.additem();
+		c1.addval(127);
+		assertTrue(127==c1.cartvalue());
+		assertTrue(1==c1.itemscount());
+	}
+	@Test
+	void test3() {
+		c1=new shopcart();
+		c1.additem();
+		c1.addval(127);
+		c1.additem();
+		c1.addval(100);
+		assertTrue(2==c1.itemscount());
+		assertTrue(227==c1.cartvalue());
+	}
+
+}
---
+++ src/shopcart.java
@@ -0,0 +1,27 @@
+
+public class shopcart {
+
+	int items=0;
+	int val=0;
+	public int itemscount() {
+		// TODO Auto-generated method stub
+		
+		return items;
+	}
+
+	public int cartvalue() {
+		// TODO Auto-generated method stub
+		return val;
+	}
+
+	public void additem() {
+		// TODO Auto-generated method stub
+		items++;
+	}
+
+	public void addval(int i) {
+		// TODO Auto-generated method stub
+		val+=i;
+	}
+
+}
